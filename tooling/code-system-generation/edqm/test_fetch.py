3079b84f57113aca4919b397b6e254b18a40186c
Francesco Galisi <f.galisi@datawizard.it>
2022-11-06 22:47:54 +0100

    Generated requirements.txt

--- tooling/code-system-generation/edqm/test_fetch.py
+++ tooling/code-system-generation/edqm/test_fetch.py
@@ -4,7 +4,7 @@ import urllib.parse
 import generate
 
 class TestFetch(unittest.TestCase):
-    def test_do_request(self):
+    def test_generate_api_key(self):
         url = urllib.parse.urlparse('https://standardterms.edqm.eu:443/standardterms/api/v1/classes')
         api_key, http_date = generate.generate_api_key(url, 'foo@bar.org', 'terriblepassword', 'Sun, 06 Nov 2022 17:45:20 GMT')
 
